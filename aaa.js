21f518ca28281be9f264bd3a8a66f1ced5115d9e
ryu euni <euni.ryu97@gmail.com>
2023-02-24 15:15:54 +0900

    js파일에서 함수만들기

--- aaa.js
+++ aaa.js
@@ -4,3 +4,11 @@ console.log(poketmon);
 for(let i = 0; i < poketmon.length; i++){
   console.log(poketmon[i]);
 }
+
+function nameTag(name){
+  return "안녕하세요" + name + "입니다";
+}
+
+function percent(realVal, totalVal){
+  return (realVal / totalVal) * 100;
+}
